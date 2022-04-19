9f0ae163fd6494cd51be1e3e385505a10c11683e
hidden9 <ali666st@gmail.com>
2022-04-19 23:21:28 +0430

    create models and config backend by hidden9

--- Backend/core/admin.py
+++ Backend/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Room , Booking , TimeSlot , User
 # Register your models here.
+
+
+
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('time_from', 'time_to', 'room_id')
+
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('date', 'customer', 'time_slot')
+
+
+
+admin.site.register(Room)
+admin.site.register(TimeSlot, TimeSlotAdmin)
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(User)
--- Backend/core/models.py
+++ Backend/core/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_room_manager = models.BooleanField(default=False)
+
+
+class Room(models.Model):
+    name = models.TextField(null=True)
+    num_days_in_adv = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class TimeSlot(models.Model):
+    time_from = models.TimeField(max_length=4)
+    time_to = models.TimeField(max_length=4)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+class Booking(models.Model):
+    date = models.DateField()
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+
+
