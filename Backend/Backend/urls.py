9570eeb7f9e3f1a91ff0b18f371aa074921d963b
nima abdpour <nimaabdpoor@gmail.com>
2022-04-26 10:34:05 +0430

    room booking urls created.

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -15,7 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from core import views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # login
+    path('api/login', obtain_auth_token, name="login"),
+    path('api/signup', views.Signup.as_view(), name="signup"),
+    path('api/profile', views.Profile.as_view(), name="profile"),
+    
+    # room booking
+    path('api/rooms', views.Rooms.as_view(), name="rooms"),
+    path('api/rooms/<int:id>', views.RoomsRUD.as_view(), name="rooms_rud"),
+    path('api/timeslots', views.TimeSlots.as_view(), name="timeslots"),
+    path('api/timeslots/<int:id>', views.TimeSlotsRD.as_view(), name="timeslots_rd"),
+    path('api/bookings', views.Bookings.as_view(), name="bookings"),
+    path('api/bookings/<int:id>', views.BookingsRD.as_view(), name="bookings_rd"),
 ]
