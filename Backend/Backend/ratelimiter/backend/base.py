76f3a1da25ee111e200a98d6f430e817b7eb078c
nima abdpour <nimaabdpoor@gmail.com>
2022-05-22 18:07:00 +0430

    add ratelimiter files

--- Backend/Backend/ratelimiter/backend/base.py
+++ Backend/Backend/ratelimiter/backend/base.py
@@ -21,3 +21,20 @@ class SlidingWindow:
         self.cur_time = time()
         self.pre_count = capacity
         self.cur_count = 0
+
+    
+
+    def handle(self, packet):
+
+        if (time() - self.cur_time) > self.time_unit:
+            self.cur_time = time()
+            self.pre_count = self.cur_count
+            self.cur_count = 0
+
+        ec = (self.pre_count * (self.time_unit - (time() - self.cur_time)) / self.time_unit) + self.cur_count
+
+        if (ec > self.capacity):
+            return self.drop_callback(packet)
+
+        self.cur_count += 1
+        return self.forward_callback(packet)
